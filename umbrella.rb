0e4f3fd06be5df86c233bfe45ccc5e600f31718a
Akshleen Dhanoa <161362298+akshleen@users.noreply.github.com>
2025-01-27 21:50:05 +0000

    created key value pairing

--- umbrella.rb
+++ umbrella.rb
@@ -1,8 +1,4 @@
 require "http"
 require "json"
 require 'dotenv/load'
-# /.env
-GMAPS_KEY="AIzaSyDKz4Y3bvrTsWpPRNn9ab55OkmcwZxLOHI"
 
-
-PIRATE_WEATHER_KEY="3RrQrvLmiUayQ84JSxL8D2aXw99yRKlx1N4qFDUE"
