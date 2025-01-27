98c4c83478ba51132ae55b62eedc5d8d2dc33e90
Akshleen Dhanoa <161362298+akshleen@users.noreply.github.com>
2025-01-27 22:38:26 +0000

    added geocode and weather data

--- umbrella.rb
+++ umbrella.rb
@@ -1,4 +1,34 @@
-require "http"
-require "json"
-require 'dotenv/load'
+require 'google_maps_service'
+require 'httparty'
+require 'ascii_charts'
 
+puts "========================================"
+puts "    Will you need an umbrella today?    "
+puts "========================================"
+puts "\nWhere are you?"
+location = gets.chomp
+gmaps = GoogleMapsService::Client.new(key: GMAPS_KEY)
+
+# Geocode the location
+results = gmaps.geocode(location)
+if results.empty?
+  puts "Could not find the location."
+  exit
+end
+
+coordinates = results[0][:geometry][:location]
+latitude = coordinates[:lat]
+longitude = coordinates[:lng]
+
+puts "\nChecking the weather at #{location.capitalize}...."
+puts "Your coordinates are #{latitude}, #{longitude}."
+
+# Get weather data
+response = HTTParty.get("https://api.pirateweather.net/forecast/#{PIRATE_WEATHER_KEY}/#{latitude},#{longitude}")
+weather_data = response.parsed_response
+
+current_temp = weather_data['currently']['temperature']
+hourly_data = weather_data['hourly']['data']
+
+puts "It is currently #{current_temp}°F."
+puts "Next hour: #{weather_data['minutely'] ? weather_data['minutely']['summary'] : 'No minute data available.'}"
